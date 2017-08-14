d5a4749bf5ad9085b5fe919b66515bdabb2cbed0
Carol1992 <1689597038@qq.com>
2017-08-14 18:28:08 +0800

    commit

--- index.js
+++ index.js
@@ -56,3 +56,42 @@ document.getElementById("app").append(`
 //第三个参数：5
 var a = 10, b = 5;
 tag`Hello ${a+b} world ${a-b}`;
+function tag(s, v1, v2){
+	console.log(s[0]);
+	console.log(s[1]);
+	console.log(v1 + v2);
+}
+
+var pattern = /[0-9]{4}([0-9]{2})([0-9]{2})/;
+var groups = pattern.exec("19920907");
+console.log("生日：" + groups[1] + "月" + groups[2] + "日");
+
+var stocks = [
+	{颜色:"红", 尺码:"L", 价格:200, 厚度:"厚", skuId:"00001"},
+	{颜色:"蓝", 尺码:"M", 价格:300, 厚度:"中厚", skuId:"00002"},
+	{颜色:"黑", 尺码:"S", 价格:120, 厚度:"薄", skuId:"00003"},
+	{颜色:"红", 尺码:"XL", 价格:210, 厚度:"厚", skuId:"00004"}
+];
+var stock_groups = {
+	colors:[],
+	sizes:[],
+	prices:[],
+	type:[]
+};
+for(let i=0; i<stocks.length; i++){
+	var {颜色, 尺码, 价格, 厚度, skuId} = stocks[i];
+	stock_groups.colors.push(颜色);
+	stock_groups.sizes.push(尺码);
+	stock_groups.prices.push(价格);
+	stock_groups.type.push(厚度);
+}
+var stock_groups2 = {
+	colors: new Set(stock_groups.colors),
+	sizes:new Set(stock_groups.sizes),
+	prices:new Set(stock_groups.prices),
+	type:new Set(stock_groups.type)
+}
+console.log(stock_groups2);
+for(let v of stock_groups2.colors){
+	console.log(v);
+}
